4f79b8b4595916ca7671a138e86fe2f260bf9ffd
viktkey <100194509+viktkey@users.noreply.github.com>
2022-02-22 16:08:08 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+     <a href="https://reactdev.ru/libs/react-router/#_2">Основная мартшрутизация</a>
+      <div className="block1">
+      <a href="https://github.com/remix-run/history">Управление историей сеансов</a>
+    <div className="block2">
+      <a href="https://reactdev.ru/handbook/integrating-with-other-libraries/">Взаимодействие со сторонними библиотеками</a>
+    </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
